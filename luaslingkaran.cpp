17814dbc2fa2b5efea547fe5e7a34c6287d73c0a
fazila7002 <fazilatunnisa705@gmail.com>
2026-03-03 21:52:34 +0700

    menambahkan library dan namespace

--- luaslingkaran.cpp
+++ luaslingkaran.cpp
@@ -1,23 +1,3 @@
 #include <iostream>
 using namespace std;
 
-float jarijari;
-float phi = 3.14159;
-
-void awal(){
-cout << "Masukkan jarijari lingkaran=";
-cin >> jarijari;
-}
-
-float luas (float jari_jari,float phi){
-    return phi*jarijari*jarijari;
-}
-
-void akhir (){
-    cout << "Hasil luas untuk lingkaran dengan jari jari" << jarijari <<"adalah=" << luas (jarijari,phi); 
-}
-
-int main(){
-    awal();
-    akhir();
-}
