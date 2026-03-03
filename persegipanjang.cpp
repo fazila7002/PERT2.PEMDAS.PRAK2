cc8f755bb0208b2654f10fc84397782c5e2fda7f
fazila7002 <fazilatunnisa705@gmail.com>
2026-03-03 21:19:53 +0700

    implementasi prosedur dan fungsi

--- persegipanjang.cpp
+++ persegipanjang.cpp
@@ -2,3 +2,22 @@
 using namespace std;
 
 int p, l, luas;
+
+void input (){
+    cout << "Masukkan Panjang =";
+    cin >> p;
+    cout << "Masukkan Lebar =";
+    cin >> l;
+}
+
+int luasPersegi(){
+    return p * l;
+
+}
+
+void output(){
+    cout << "Hasilnya =" << luasPersegi() << endl;
+    cout << "Terimakasih";
+
+    
+}
